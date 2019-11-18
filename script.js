e666f9f1a8ae4a8956e2bd87c9e4e6325da214e2
Ginner Zapata <ginnerzapata@gmail.com>
2019-11-18 18:09:37 -0300

    creating basic functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+let oriBoard;
+const huPlayer = '0';
+const aiPlayer = 'X';
+const winCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+];
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+  document.querySelector('.endgame').style.display='none';
+  oriBoard = Array.from(Array(9).keys());
+  
+  for(let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click',turnClick, false);
+  }
+};
+
+function turnClick(square){
+  console.log(square.target.id)
+}
